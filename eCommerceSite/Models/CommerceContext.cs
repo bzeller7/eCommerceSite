1c03c168bb5d0e04b0c2d17dec9225441d379f67
bzeller7 <zeller8964@student.cptc.edu>
2019-01-24 09:38:46 -0800

    Added more content to eCommerceSite.Models

---
+++ eCommerceSite/Models/CommerceContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class CommerceContext : DbContext
+    {
+        public CommerceContext(DbContextOptions<CommerceContext> options)
+            : base(options)
+        {
+
+        }
+        
+        //Add all Models (classes) as a DbSet that need
+        //to be tracked by the database
+        public DbSet<Member> Members { get; set; }
+    }
+}
---
+++ eCommerceSite/Models/Member.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eCommerceSite.Models
+{
+    public class Member
+    {
+        //Data annotations - Validation and DB mark up
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        //int is automatically required because
+        //it is a value type
+        [Key] //Mark this field as PK (Primary Key)
+        public int MemberId { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
